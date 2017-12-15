10e6f371c4ad661714b6061117a407e58577cc73
Viktor Malik <viktor.malik@gmail.com>
2017-12-15 09:49:24 +0100

    Size of opaque struct type is 1.

--- reve/reve/lib/Helper.cpp
+++ reve/reve/lib/Helper.cpp
@@ -81,6 +81,10 @@ SMTRef instrNameOrVal(const llvm::Value *val, const llvm::Type *ty) {
 }
 
 int typeSize(llvm::Type *Ty, const llvm::DataLayout &layout) {
+    if (auto StructTy = llvm::dyn_cast<llvm::StructType>(Ty)) {
+        if (StructTy->isOpaque())
+            return 1;
+    }
     if (SMTGenerationOpts::getInstance().ByteHeap == ByteHeapOpt::Enabled) {
         return static_cast<int>(layout.getTypeAllocSize(Ty));
     }
